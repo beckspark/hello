9a9ac814da742cb07fa47ed0a7eedce7446650fb
beckspark <stephenpbeck@gmail.com>
2025-09-24 07:54:14 -0400

    Add 404 response.

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     if request_line == "GET / HTTP/1.1" {
         let status_line = "HTTP/1.1 200 OK";
-        let contents = fs.read_to_string("hello.xhtml").unwrap();
+        let contents = fs::read_to_string("hello.xhtml").unwrap();
         let length = contents.len();
 
         let response = format!(
@@ -35,6 +35,16 @@ fn handle_connection(mut stream: TcpStream) {
 
         stream.write_all(response.as_bytes()).unwrap();
     } else {
-        // some other request
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.xhtml").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\n
+            Content-Length: {length}\r\n\r\n
+            {contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
