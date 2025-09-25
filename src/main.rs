85895258dfb321ba4c7939ae1e8ba7db8c513292
beckspark <stephenpbeck@gmail.com>
2025-09-25 07:40:25 -0400

    Refactor if/else.

--- src/main.rs
+++ src/main.rs
@@ -22,29 +22,20 @@ fn handle_connection(mut stream: TcpStream) {
         .unwrap()
         .unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.xhtml").unwrap();
-        let length = contents.len();
+    let (status_line, filename) =
+        if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "hello.xhtml")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.xhtml")
+        };
 
-        let response = format!(
-            "{status_line}\r\n\
-            Content-Length: {length}\r\n\r\n\
-            {contents}"
-        );
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!(
+        "{status_line}\r\n
+        Content-Length: {length}\r\n\r\n\
+        {contents}"
+    );
 
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.xhtml").unwrap();
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\n
-            Content-Length: {length}\r\n\r\n
-            {contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    stream.write_all(response.as_bytes()).unwrap();
 }
